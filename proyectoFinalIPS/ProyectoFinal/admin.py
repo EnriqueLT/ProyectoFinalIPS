cfc7ea6f7d781e4b951a2642be4d4596032b8d3d
jhonpanocca <jhon.panocca@gmail.com>
2021-06-30 10:28:02 -0500

    Nuevo Proyecto con Google auth
    Implementación de login con google

--- proyectoFinalIPS/ProyectoFinal/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Paciente,Doctor,Cita
-
-admin.site.register(Paciente)
-admin.site.register(Doctor)
-admin.site.register(Cita)
